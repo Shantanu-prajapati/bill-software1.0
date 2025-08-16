8c11dfc16644d1c53a1253bed43c8bc6d4051b59
Shantanu-prajapati <159682238+Shantanu-prajapati@users.noreply.github.com>
2025-08-16 12:50:40 +0530

    Add link

--- src/main/java/in/shani/billingsoftware/config/SecurityConfig.java
+++ src/main/java/in/shani/billingsoftware/config/SecurityConfig.java
@@ -58,7 +58,7 @@ private final JwtRequestFilter jwtRequestFilter;
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config =new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://billing-software-h83olvfci-shantanu-prajapatis-projects.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
